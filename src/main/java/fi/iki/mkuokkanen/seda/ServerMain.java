a0926a91662e2e81bee1b1a461ac2d6c77294287
Mikko Kuokkanen <mikko.kuokkanen@iki.fi>
2014-03-05 21:34:40 +0200

    Fixed bug, on guice module was missing

--- src/main/java/fi/iki/mkuokkanen/seda/ServerMain.java
+++ src/main/java/fi/iki/mkuokkanen/seda/ServerMain.java
@@ -9,6 +9,7 @@ import com.google.inject.Injector;
 import fi.iki.mkuokkanen.seda.api.WebsocketModule;
 import fi.iki.mkuokkanen.seda.keyStore.StorageModule;
 import fi.iki.mkuokkanen.seda.queue.QueueModule;
+import fi.iki.mkuokkanen.seda.timed.SchedulerModule;
 
 /**
  * Main Application to start server.
@@ -17,13 +18,16 @@ public class ServerMain {
 
     private static Logger logger = LoggerFactory.getLogger(ServerMain.class);
 
+    private static Injector injector;
+
     public static void main(String[] args) {
         logger.info("Starting application");
 
-        Injector injector = Guice.createInjector(
+        injector = Guice.createInjector(
                 new WebsocketModule(),
                 new QueueModule(),
-                new StorageModule()
+                new StorageModule(),
+                new SchedulerModule()
                 );
 
         ServerApp api = injector.getInstance(ServerApp.class);
